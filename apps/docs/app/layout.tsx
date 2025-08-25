0c3a9b09bbc0984bc69fa1b32f3398712b4b876a
Zlatko Fedor <zfedor@gmail.com>
2025-08-25 15:47:00 +0200

    add clarity (#239)

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -20,6 +21,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <Script id="clr-script">
+        {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "t0do1neexj");`}
+        </Script>
+      </head>
       <body className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
         {children}
       </body>
